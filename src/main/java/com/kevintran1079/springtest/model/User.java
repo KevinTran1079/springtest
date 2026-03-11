c3f5f905cb4674326beb4ed636f4a342ea55227b
Kevin Tran <187645416+KevinTran1079@users.noreply.github.com>
2026-03-10 22:05:25 -0500

    added implementation for /api/users respecting the openapi contract

---
+++ src/main/java/com/kevintran1079/springtest/model/User.java
@@ -0,0 +1,16 @@
+package com.kevintran1079.springtest.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class User {
+    private Long id;
+    private String username;
+    private String password;
+    private String email;
+}
